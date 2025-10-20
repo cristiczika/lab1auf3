aee2a36b76c6d0e163074d86955d801bec6b08a9
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:23:06 +0300

    1)

--- src/main/tasks/NumberArrayUtils.java
+++ src/main/tasks/NumberArrayUtils.java
@@ -1,4 +1,35 @@
 package main.tasks;
 
 public class NumberArrayUtils {
+
+    private static int[] removeZeros(int[] array) {
+        int i = 0;
+
+        while (i < array.length - 1 && array[i] == 0) {
+            i++;
+        }
+
+        int[] res = new int[array.length - i];
+        for (int j = 0; j < res.length; j++) {
+            res[j] = array[i + j];
+        }
+
+        return res;
+    }
+
+    public static int[] add(int[] a, int[] b) {
+        int n = a.length;
+        int[] result = new int[n + 1];
+        int carry = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = a[i] + b[i] + carry;
+            result[i + 1] = sum % 10;
+            carry = sum / 10;
+        }
+
+        result[0] = carry;
+        return removeZeros(result);
+    }
+
 }
