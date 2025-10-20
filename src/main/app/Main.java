775959d7b791e26e08d51e8e7f09ee68fe57216f
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:24:38 +0300

    Main

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -1,9 +1,20 @@
 package main.app;
 
+import main.tasks.NumberArrayUtils;
+
+import java.util.Arrays;
+
 public class Main {
 
     static void main() {
-
+        IO.println("------- Aufgabe 3 --------");
+        int[] a = {1, 3, 0, 0, 0, 0, 0, 0, 0}, b = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        IO.println(Arrays.toString(NumberArrayUtils.add(a, b)));
+        int[] c = {8, 3, 0, 0, 0, 0, 0, 0, 0}, d = {5, 4, 0, 0, 0, 0, 0, 0, 0};
+        IO.println(Arrays.toString(NumberArrayUtils.subtract(c, d)));
+        int[] e = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        IO.println(Arrays.toString(NumberArrayUtils.multiply(e, 2)));
+        IO.println(Arrays.toString(NumberArrayUtils.divide(e, 2)));
     }
 
 }
