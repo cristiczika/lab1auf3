86b98cd7fa04d6005e915c3523a8ac868a80c4a5
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:23:27 +0300

    2)

--- src/main/tasks/NumberArrayUtils.java
+++ src/main/tasks/NumberArrayUtils.java
@@ -32,4 +32,23 @@ public class NumberArrayUtils {
         return removeZeros(result);
     }
 
+    public static int[] subtract(int[] a, int[] b) {
+        int n = a.length;
+        int[] result = new int[n];
+        int borrow = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int diff = a[i] - b[i] - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            result[i] = diff;
+        }
+
+        return removeZeros(result);
+    }
+
 }
