1f1ee06167a6356bfb2a2d3c56ca06e79636819b
cristiczika <cristianczika6@gmail.com>
2025-10-20 18:23:43 +0300

    3)

--- src/main/tasks/NumberArrayUtils.java
+++ src/main/tasks/NumberArrayUtils.java
@@ -51,4 +51,19 @@ public class NumberArrayUtils {
         return removeZeros(result);
     }
 
+    public static int[] multiply(int[] a, int digit) {
+        int n = a.length;
+        int[] result = new int[n + 1];
+        int carry = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int prod = a[i] * digit + carry;
+            result[i + 1] = prod % 10;
+            carry = prod / 10;
+        }
+
+        result[0] = carry;
+        return removeZeros(result);
+    }
+
 }
